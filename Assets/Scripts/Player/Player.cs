d4edd506b07e66462a04660fb4c7862b45e6c360
Alex Duh <alexduh@gmail.com>
2025-01-17 18:48:37 -0500

    added some file structure

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : Singleton<Player>
+{
+
+    //Player.Instance.Initialize();
+
+    float MOVE_SPEED = 30f;
+    Vector2 cursorPos;
+
+    void Initialize()
+    {
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    float FIRE_RATE;
+    float DAMAGE;
+    float MAX_AMMO;
+    float CURR_AMMO;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
